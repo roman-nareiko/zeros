14d5574ba6a0828f38100853edfcfc7ba89c51e8
Raman Nareika <Raman.Nareika@thomsonreuters.com>
2018-02-23 18:12:22 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports = function zeros(number) {
+  let fives = 0;
+
+  function count_fives(number){
+    let count = 0;
+
+    while(number % 5 === 0){
+      count++;
+      number /= 5;
+    }
+
+    return count;
+  }
+
+  for(let i = 5; i <= number; i += 5) {
+    fives += count_fives(i);
+  }
+
+  return fives;
 }
